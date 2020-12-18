5b7f9dbfd31d01370b9303bf2688d1ac4f23cf90
Sapan <sapan.jain@lntinfotech.com>
2020-12-18 10:20:06 +0530

    cmt

--- MyProj/src/College.java
+++ MyProj/src/College.java
@@ -4,6 +4,8 @@ public class College {
 	public static void main(String[] args) {
 		System.out.println("My College is MITS gwalior");
 		System.out.println("Gwalior fort");
+		System.out.println("Gwalior");
+		
 		
 		int r= 10;
 	}
